c4257adf85024c9fa73c677d592be8201be4b0e2
Julien PRIGENT <julien.prigent@fnacdarty.com>
2022-12-16 09:08:21 +0100

    Update 16/12/2022

---
+++ src/Entity/Table.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TableRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TableRepository::class)
+ * @ORM\Table(name="`table`")
+ */
+class Table
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $width;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWidth(): ?int
+    {
+        return $this->width;
+    }
+
+    public function setWidth(int $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+}
